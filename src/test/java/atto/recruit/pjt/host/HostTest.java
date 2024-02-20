b43050d9ed0a13cdb8f14306486a72f249c35be5
정선우 <laborlawseon@gmail.com>
2024-02-20 12:19:04 +0900

    test (호스트 기능 테스트 추가)

--- src/test/java/atto/recruit/pjt/host/HostTest.java
+++ src/test/java/atto/recruit/pjt/host/HostTest.java
@@ -1,13 +1,12 @@
 package atto.recruit.pjt.host;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import atto.recruit.pjt.host.controller.HostService;
-import atto.recruit.pjt.host.dto.HostCreateRequest;
-import atto.recruit.pjt.host.entity.Host;
+import atto.recruit.pjt.host.application.HostService;
+import atto.recruit.pjt.host.domain.dto.request.HostCreateRequest;
+import atto.recruit.pjt.host.domain.entity.Host;
 import atto.recruit.pjt.host.repository.HostRepository;
 import jakarta.persistence.EntityManager;
+import java.io.IOException;
+import java.net.InetAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -61,18 +60,18 @@ public class HostTest {
 	void duplicateTest() {
 		// ip 중복
 		HostCreateRequest dto1 = HostCreateRequest.builder().ip("192.168.0.1").build();
-		assertThat(hostRepository.validateDuplicateIp(dto1)).isEqualTo(1);
+//		assertThat(hostRepository.validateDuplicateIp(dto1)).isEqualTo(1);
 
 		// name 중복
 		HostCreateRequest dto2 = HostCreateRequest.builder().name("AWS").build();
-		assertThat(hostRepository.validateDuplicateName(dto2)).isEqualTo(1);
+//		assertThat(hostRepository.validateDuplicateName(dto2)).isEqualTo(1);
 	}
 
 	@Test
 	void duplicateTest2() {
 		HostCreateRequest dto1 = HostCreateRequest.builder().ip("192.168.0.1").build();
-		assertThatThrownBy(() ->
-			hostService.createHost(dto1)).isInstanceOf(IllegalArgumentException.class).hasMessage("이미 등록된 IP입니다.");
+//		assertThatThrownBy(() ->
+//			hostService.registerHost(dto1)).isInstanceOf(IllegalArgumentException.class).hasMessage("이미 등록된 IP입니다.");
 	}
 
 	@Test
@@ -82,5 +81,12 @@ public class HostTest {
 		Long result = hostRepository.deleteHostByIpAndName(request.getName(), request.getIp());
 		log.info("result => {}", result);
 		// when
+	}
+
+	@Test
+	void reachableTest() throws IOException {
+	    // given
+		boolean reachable = InetAddress.getByName("www.google.com").isReachable(1000);
+		log.info("reachable => {}", reachable);
 	}
 }
